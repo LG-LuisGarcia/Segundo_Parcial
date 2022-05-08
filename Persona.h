3a41609093e17698ab1e8fe28a77416254816faf
Luis García <89419290+LG-LuisGarcia@users.noreply.github.com>
2022-05-07 20:58:52 -0600

    Add files via upload

---
+++ Persona.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Persona
+
+{
+protected: string nombres, apellidos, direccion, telefono, genero, fecha_nacimiento;
+
+		 // costructor
+protected:
+	Persona() {
+	}
+	Persona(string nom, string ape, string dir, string tel, string gn, string fn) {
+		nombres = nom;
+		apellidos = ape;
+		direccion = dir;
+		telefono = tel;
+		genero = gn;
+		fecha_nacimiento = fn;
+	}
+};
+
